601529a7aeebfc4933cd40c07acfb5c2805dc559
PavelPotapov <pavelpotapovv96@gmail.com>
2020-01-06 19:38:45 +0500

    функции заполнения поля и хода + новый заголовочный файл
    в файле sapcer.cpp добавил функции заполнения поля(полец цифр) и функцию высрела(хода) с прорисовкой картинок, так же создал новый заголовочный файл в который помещу прототип одной функции

--- Project/saper.cpp
+++ Project/saper.cpp
@@ -85,4 +85,78 @@ void func(int x, int y){
       }
    }
 }
+//-----ход (попал или не попал, прорисовывание картинки)-------------------------------
+bool shot(int x, int y, int a, int b){
+   if (y>=17 && y<=22 && x>=12 && x<=16) return false;
+   else if (x>15 || x<0 || y<0 || y>15) return true;//проверка на границы поля
+   else if (mousebuttons()==1 ){//для ЛКМ
+      if (pole_mine[x][y]==1) {
+         for (int i=0; i<N; i++){//------------отображает все мины------
+            for(int j=0; j<N; j++){
+               if (pole_mine[i][j]==1){
+                  a=(i+1)*50;
+                  b=(j+1)*50;
+                  IMAGE * bitmap=loadBMP("Resurses/KletkaM.jpg");
+                  putimage(b,a, bitmap, COPY_PUT);
+               }
+            }
+         }
+         IMAGE * bitmap=loadBMP("Resurses/Proigral.jpg");
+         putimage(875,105, bitmap, COPY_PUT);
+         getch();
+         return false;   //  если попал в мину
+      }
+      if (pole_zero[x][y]==9) func(x,y);
+      else if (pole_zero[x][y]==1){
+          IMAGE * bitmap=loadBMP("Resurses/Kletka1.jpg");
+         putimage((y+1)*50,(x+1)*50, bitmap, COPY_PUT);
+      }
+      else if (pole_zero[x][y]==2){
+          IMAGE * bitmap=loadBMP("Resurses/Kletka2.jpg");
+         putimage((y+1)*50,(x+1)*50, bitmap, COPY_PUT);
+      }
+      else if (pole_zero[x][y]==3){
+         IMAGE * bitmap=loadBMP("Resurses/Kletka3.jpg");
+         putimage((y+1)*50,(x+1)*50, bitmap, COPY_PUT);
+      }
+      else if (pole_zero[x][y]==4){
+          IMAGE * bitmap=loadBMP("Resurses/Kletka4.jpg");
+         putimage((y+1)*50,(x+1)*50, bitmap, COPY_PUT);
+      }
+      else if (pole_zero[x][y]==5){
+          IMAGE * bitmap=loadBMP("Resurses/Kletka5.jpg");
+         putimage((y+1)*50,(x+1)*50, bitmap, COPY_PUT);
+      }
+      else if (pole_zero[x][y]==6){
+          IMAGE * bitmap=loadBMP("Resurses/Kletka6.jpg");
+         putimage((y+1)*50,(x+1)*50, bitmap, COPY_PUT);
+      }
+      else if (pole_zero[x][y]==7){
+          IMAGE * bitmap=loadBMP("Resurses/Kletka7.jpg");
+         putimage((y+1)*50,(x+1)*50, bitmap, COPY_PUT);
+      }
+      else if (pole_zero[x][y]==8){
+         IMAGE * bitmap=loadBMP("Resurses/Kletka8.jpg");
+         putimage((y+1)*50,(x+1)*50, bitmap, COPY_PUT);
+      }
+   }  
+   else if (mousebuttons()==2 ){//для ПКМ
+      if (pole_flag[x][y]==0){
+         IMAGE * bitmap=loadBMP("Resurses/Kletka11.jpg");
+         putimage((y+1)*50,(x+1)*50, bitmap, COPY_PUT);
+         pole_flag[x][y]=1;
+         if (pole_flag[x][y]==pole_mine[x][y]) kol_mine-=1;
+      }
+      else if (pole_flag[x][y]==1){
+         IMAGE * bitmap=loadBMP("Resurses/Kletka10.jpg");
+         putimage((y+1)*50,(x+1)*50, bitmap, COPY_PUT);
+         pole_flag[x][y]=0;
+         if (pole_mine[x][y]==1) kol_mine+=1;
+      }
+   }
+   return true;
+}
+
+//-------выход--------------------------------------------------------------------------
+
 
